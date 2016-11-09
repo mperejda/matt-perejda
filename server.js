43e522cc1d649d9834eb7d718c82f7def0b6251b
mperejda <matt.perejda@gmail.com>
2016-11-08 22:03:20 -0500

    cleaned up the bio

--- server.js
+++ server.js
@@ -7,10 +7,20 @@ var app = express(); 							// define our app using express
 var port = process.env.PORT || 8080;  			// set our port
 
 // LOAD PAGE ==========================================================================
+app.set('view engine', 'html');
+
 app.use(express.static('public'))
 
 app.get('/', function(req, res) {
 	res.render('../public/index')		// load a single file (angular will handle page changes on the fron end)
+});
+
+// LOAD ANOTHER PAGE ==========================================================================
+
+app.use(express.static('puzzel'))
+
+app.get('/puzzel', function(req, res) {
+	res.render('../public/puzzel')		// load another page
 });
 
 // START SERVER =======================================================================
